c397708829a1d1f3ef6b61d0133e8457677fce22
devops <devops@gmail.com>
2025-09-11 00:25:32 +0700

    update webhook nhanh

--- src/main/java/com/nemi/controller/WebhookController.java
+++ src/main/java/com/nemi/controller/WebhookController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/webhook")
+@RequestMapping("/nemi-product-manager/webhook")
 public class WebhookController {
     private static final Logger logger = LoggerFactory.getLogger(WebhookController.class);
     
