78b0e93e47d48c9ba654843265ae7c94101b761a
devops <devops@gmail.com>
2025-09-26 01:23:07 +0700

    change entity product

--- src/main/java/com/nemi/entity/ProductEntity.java
+++ src/main/java/com/nemi/entity/ProductEntity.java
@@ -15,13 +15,13 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
-@Table(name = "products")
+@Table(name = "products",schema = "product_manager")
 public class ProductEntity {
     @Id
     @Column(name = "product_id")
     private String productId;
 
-    @Column(name = "posId")
+    @Column(name = "pos_id")
     private String posId;
 
     @Column(name = "code")
