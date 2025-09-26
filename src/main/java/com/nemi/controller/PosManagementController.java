b52b984fc1cddc180d10f7d895e71b78630cf109
quyvio <nguyenducquy.hust@gmail.com>
2025-09-26 11:18:50 +0700

    refactor code

--- src/main/java/com/nemi/controller/PosManagementController.java
+++ src/main/java/com/nemi/controller/PosManagementController.java
@@ -22,7 +22,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/public-api/v1")
+@RequestMapping("/client-api/v1")
 @RequiredArgsConstructor
 public class PosManagementController {
 
