ae0ca8f5811f5c4b2908e5f4eb13321e5969fbaa
BangGiangk63 <giangnk1611@gmail.com>
2025-09-27 15:07:40 +0700

    refactor general pos

--- src/main/java/com/nemi/service/PosManagementService.java
+++ src/main/java/com/nemi/service/PosManagementService.java
@@ -1,11 +1,7 @@
 package com.nemi.service;
 
-import com.nemi.model.request.ChangeStatusRequest;
 import com.nemi.model.request.PosConnectionRequest;
 import com.nemi.model.response.PosConnectionResponse;
-import com.nemi.model.response.StatusResponse;
-
-import java.util.List;
 
 public interface PosManagementService {
     String getPosName();
