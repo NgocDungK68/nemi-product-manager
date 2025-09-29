e295c12390a3bf0a10e21d7e9f1733686287e7dd
devops <devops@gmail.com>
2025-09-29 19:55:54 +0700

    handle product update + delete

--- src/main/java/com/nemi/service_impl/nhanhvn/NhanhvnWebhookServiceImpl.java
+++ src/main/java/com/nemi/service_impl/nhanhvn/NhanhvnWebhookServiceImpl.java
@@ -1,5 +1,6 @@
 package com.nemi.service_impl.nhanhvn;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.nemi.configuration.NhanhvnConfig;
 import com.nemi.constant.enums.NhanhvnEvent;
@@ -20,6 +21,8 @@ import org.springframework.stereotype.Service;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -71,44 +74,109 @@ public class NhanhvnWebhookServiceImpl implements WebhookService {
             return false;
         }
 
-        switch (event) {
-            case WEBHOOKS_ENABLED:
-                return handleWebhooksEnabled(data);
-            case PRODUCT_ADD, PRODUCT_UPDATE:
-                return handleProductAddAndUpdate(posId, data);
-            default:
+        return switch (event) {
+            case WEBHOOKS_ENABLED -> handleWebhooksEnabled(posId, data);
+            case PRODUCT_ADD -> handleProductAdd(posId, data);
+            case PRODUCT_UPDATE -> handleProductUpdate(posId, data);
+            case PRODUCT_DELETE -> handleProductDelete(posId, data);
+            default -> {
                 log.warn("Unhandled event: {}", event);
-                return false;
-        }
+                yield false;
+            }
+        };
     }
 
-    private boolean handleWebhooksEnabled(Object data) {
+    private boolean handleWebhooksEnabled(String posId, Object data) {
         // logic
         return true;
     }
 
-    private boolean handleProductAddAndUpdate(String posId, Object data) {
+    private boolean handleProductAdd(String posId, Object data) {
         NhanhvnProductResponse.ProductData productData = objectMapper.convertValue(
                 data, NhanhvnProductResponse.ProductData.class
         );
-        log.info("ProductData: {}", productData);
+        log.info("Add ProductData: {}", productData);
 
         if (productData == null) {
-            log.error("Failed to parse product data: {}", data);
+            log.error("Failed to add product data: {}", data);
             return false;
         }
 
         if (productData.getParentId() == -2) {
             ProductEntity productEntity = nhanhvnService.convertToProductEntity(posId, productData);
             productRepository.save(productEntity);
-            log.info("Successfully add 1 product");
+            log.info("Successfully add 1 product with id={}", productEntity.getProductId());
         } else {
             ProductVariantEntity variantEntity = nhanhvnService.convertToVariantEntity(productData);
             variantRepository.save(variantEntity);
-            log.info("Successfully add 1 product variant");
+            log.info("Successfully add 1 product variant with id={}", variantEntity.getVariantId());
         }
 
         return true;
+    }
+
+    private boolean handleProductUpdate(String posId, Object data) {
+        NhanhvnProductResponse.ProductData productData = objectMapper.convertValue(
+                data, NhanhvnProductResponse.ProductData.class
+        );
+        log.info("Update ProductData: {}", productData);
+
+        if (productData == null) {
+            log.error("Failed to update product data: {}", data);
+            return false;
+        }
+
+        if (productData.getParentId() == -2) {
+            Optional<ProductVariantEntity> variantEntity = variantRepository.findById(String.valueOf(productData.getId()));
+            if (variantEntity.isPresent()) {
+                variantRepository.deleteById(String.valueOf(productData.getId()));
+                log.info("Deleted variant with id={} because it is now a parent product", variantEntity.get().getVariantId());
+            }
+
+            ProductEntity productEntity = nhanhvnService.convertToProductEntity(posId, productData);
+            productRepository.save(productEntity);
+            log.info("Successfully update 1 product with id={}", productEntity.getProductId());
+        } else {
+            Optional<ProductEntity> productEntity = productRepository.findById(String.valueOf(productData.getId()));
+            if (productEntity.isPresent()) {
+                productRepository.deleteById(String.valueOf(productData.getId()));
+                log.info("Deleted product with id={} because it is now a variant", productEntity.get().getProductId());
+            }
+
+            ProductVariantEntity variantEntity = nhanhvnService.convertToVariantEntity(productData);
+            variantRepository.save(variantEntity);
+            log.info("Successfully update 1 product variant with id={}", variantEntity.getVariantId());
+        }
+
+        return true;
+    }
+
+    /**
+     * body: {"event":"productDelete","businessId":215487,"data":["15"]}
+     */
+    private boolean handleProductDelete(String posId, Object data) {
+        List<String> ids = objectMapper.convertValue(data, new TypeReference<>() {});
+        if (ids.isEmpty()) {
+            log.warn("No product ID provided for deletion");
+            return false;
+        }
+
+        String productId = ids.get(0);
+
+        if (variantRepository.existsById(productId)) {
+            variantRepository.deleteById(productId);
+            log.info("Deleted variant with id={} from posId={}", productId, posId);
+            return true;
+        }
+
+        if (productRepository.existsById(productId)) {
+            productRepository.deleteById(productId);
+            log.info("Deleted product with id={} from posId={}", productId, posId);
+            return true;
+        }
+
+        log.warn("No product or variant found with id={} for posId={}", productId, posId);
+        return false;
     }
 
     private String readBody(HttpServletRequest request) {
