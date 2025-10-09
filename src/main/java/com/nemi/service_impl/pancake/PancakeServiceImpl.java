1a24b29c914b3d3943761c813c983d8f6125b4b2
Nemi Devops <155896816+mottocaat@users.noreply.github.com>
2025-10-09 22:54:50 +0700

    complete pancake

--- src/main/java/com/nemi/service_impl/pancake/PancakeServiceImpl.java
+++ src/main/java/com/nemi/service_impl/pancake/PancakeServiceImpl.java
@@ -9,23 +9,27 @@ import com.nemi.entity.OrderEntity;
 import com.nemi.entity.OrderItemEntity;
 import com.nemi.entity.PosEntity;
 import com.nemi.entity.ProductEntity;
+import com.nemi.entity.ProductVariantEntity;
 import com.nemi.entity.SyncHistoryEntity;
 import com.nemi.enums.PosName;
 import com.nemi.enums.PosStatus;
 import com.nemi.enums.Status;
 import com.nemi.enums.SyncErrorMessage;
+import com.nemi.enums.WeightUnit;
 import com.nemi.exception.TechnicalAlertCode;
 import com.nemi.exception.TechnicalException;
 import com.nemi.exception.pojo.AlertMessages;
 import com.nemi.model.request.PosConnectionRequest;
 import com.nemi.model.request.pancake.PancakeRequest;
 import com.nemi.model.response.PosConnectionResponse;
+import com.nemi.model.response.nhanhvn.NhanhvnProductResponse;
 import com.nemi.model.response.pancake.PancakeOrderResponse;
 import com.nemi.model.response.pancake.PancakeProductResponse;
 import com.nemi.repository.OrderItemRepository;
 import com.nemi.repository.OrderRepository;
 import com.nemi.repository.PosRepository;
 import com.nemi.repository.ProductRepository;
+import com.nemi.repository.ProductVariantRepository;
 import com.nemi.repository.SyncHistoryRepository;
 import com.nemi.service.PosManagementService;
 import com.nemi.util.ClaimUtil;
@@ -35,6 +39,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.ObjectUtils;
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.security.access.method.P;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
@@ -60,6 +65,7 @@ public class PancakeServiceImpl implements PosManagementService {
     private final SyncHistoryRepository syncHistoryRepository;
     private final OrderItemRepository orderItemRepository;
     private final PancakeConfig pancakeConfig;
+    private final ProductVariantRepository productVariantRepository;
 
     private int orderBatchSize;
     private int orderItemBatchSize;
@@ -134,6 +140,7 @@ public class PancakeServiceImpl implements PosManagementService {
             }
 
             List<ProductEntity> allProducts = new ArrayList<>();
+            List<ProductVariantEntity> allVariants = new ArrayList<>();
             int pageNumber = pageStartNumber;
 
             PancakeRequest request = PancakeRequest.builder()
@@ -163,6 +170,9 @@ public class PancakeServiceImpl implements PosManagementService {
                 List<ProductEntity> pageProducts = convertToProductEntities(posId, response.getData());
                 allProducts.addAll(pageProducts);
 
+                List<ProductVariantEntity> pageVariants = convertToVariantEntities(posId, response.getData());
+                allVariants.addAll(pageVariants);
+
                 if (ObjectUtils.isEmpty(response.getData())) {
                     log.info("No products found with page number: {}", pageNumber);
                     break;
@@ -174,6 +184,7 @@ public class PancakeServiceImpl implements PosManagementService {
 
             syncHistoryRepository.save(toSyncHistory(history, null, true));
             saveAllProductsSync(allProducts);
+            saveAllVariantsSync(allVariants);
             log.info("Successfully synced {} products from Pancake", allProducts.size());
             return true;
         } catch (Exception e) {
@@ -218,6 +229,33 @@ public class PancakeServiceImpl implements PosManagementService {
                 .collect(Collectors.toList());
     }
 
+    public void saveAllVariantsSync(List<ProductVariantEntity> variants) {
+        log.info("[NhanhvnServiceImpl.saveAllVariantsSync] Saving {} Nhanh.vn variants synchronously", variants.size());
+
+        if (variants.isEmpty()) {
+            log.info("[NhanhvnServiceImpl.saveAllVariantsSync] No variants to save.");
+            return;
+        }
+
+        try {
+            int batchSize = 50;
+            for (int i = 0; i < variants.size(); i += batchSize) {
+                int endIndex = Math.min(i + batchSize, variants.size());
+                List<ProductVariantEntity> batch = variants.subList(i, endIndex);
+
+                productVariantRepository.saveAll(batch);
+                log.info("[NhanhvnServiceImpl.saveAllVariantsSync] Saved batch {}-{} of {} variants",
+                        i + 1, endIndex, variants.size());
+            }
+
+            log.info("[NhanhvnServiceImpl.saveAllVariantsSync] Successfully saved all {} Nhanh.vn variants", variants.size());
+        } catch (Exception e) {
+            log.error("[NhanhvnServiceImpl.saveAllVariantsSync] Failed to save Nhanh.vn variants synchronously: {}", e.getMessage(), e);
+            throw new TechnicalException(AlertMessages.alert(TechnicalAlertCode.DATA_PERSISTENCE_ERROR));
+        }
+    }
+
+
     private ProductEntity convertToProductEntity(String posId, PancakeProductResponse.ProductData apiProducts) {
         ProductEntity product = new ProductEntity();
         product.setPosId(posId);
@@ -243,6 +281,16 @@ public class PancakeServiceImpl implements PosManagementService {
         syncHistoryEntity.setEndTime(LocalDateTime.now());
         return syncHistoryEntity;
     }
+
+    private List<ProductVariantEntity> convertToVariantEntities(String posId, List<PancakeProductResponse.ProductData> apiProducts) {
+        return apiProducts.stream()
+                .map(apiProduct -> convertToVariantEntity(posId, apiProduct))
+                .filter(Objects::nonNull)
+                .toList();
+    }
+
+
+
 
     private List<OrderEntity> convertToOrderEntities(String posId, List<PancakeOrderResponse.DataItem> apiOrders) {
         return apiOrders.stream()
@@ -305,6 +353,30 @@ public class PancakeServiceImpl implements PosManagementService {
         }
         return orderItemEntities;
     }
+    public ProductVariantEntity convertToVariantEntity(String posId, PancakeProductResponse.ProductData apiProduct) {
+
+        Integer remainQuantity = Optional.ofNullable(apiProduct.getVariationsWarehouses())
+                .filter(list -> !list.isEmpty())
+                .map(list -> list.get(0))
+                .map(PancakeProductResponse.VariationWarehouse::getRemainQuantity)
+                .orElse(null);
+
+
+        return ProductVariantEntity.builder()
+                .variantId(apiProduct.getId())
+                .posId(posId)
+                .productId(String.valueOf(apiProduct.getProductId()))
+                .sku(apiProduct.getDisplayId())
+                .barcode(apiProduct.getBarcode())
+                .price(BigDecimal.valueOf(apiProduct.getRetailPrice()))
+                .inventoryQuantity(apiProduct.getRemainQuantity())
+                .fulfillableQuantity(remainQuantity)
+                .weight(apiProduct.getWeight())
+                .weightUnit(WeightUnit.GAM.getValue())
+                .build();
+    }
+
+
 
     public void saveAllOrdersSync(List<OrderEntity> orderEntities) {
         log.info("Saving {} Pancake orders synchronously", orderEntities.size());
@@ -414,7 +486,7 @@ public class PancakeServiceImpl implements PosManagementService {
                 List<OrderItemEntity> pageOrderItems = convertToOrderItemEntities(response.getData());
                 allOrderItems.addAll(pageOrderItems);
 
-                if (request.getPageNumber() > 2) {
+                if (ObjectUtils.isEmpty(response.getData())) {
                     log.info("No orders found with page number: {}", pageNumber);
                     break;
                 } else {
