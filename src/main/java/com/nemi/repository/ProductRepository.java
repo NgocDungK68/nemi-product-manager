84df8b11e269f2a8f1d61e2e13337e7529b5ff84
devops <you@example.com>
2025-10-15 03:09:35 +0700

    refractor handleProductDelete + code inventoryChange event

--- src/main/java/com/nemi/repository/ProductRepository.java
+++ src/main/java/com/nemi/repository/ProductRepository.java
@@ -3,9 +3,6 @@ package com.nemi.repository;
 import com.nemi.entity.ProductEntity;
 import com.nemi.entity.ProductId;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
