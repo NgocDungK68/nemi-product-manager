962fc78ea95c6e3bd996a7a2af806b05140e146b
Nemi Devops <155896816+mottocaat@users.noreply.github.com>
2025-10-21 16:22:00 +0700

    done pancake and nhanhvn

--- src/main/java/com/nemi/annotation/NhanhvnAuthChecker.java
+++ src/main/java/com/nemi/annotation/NhanhvnAuthChecker.java
@@ -25,12 +25,6 @@ public class NhanhvnAuthChecker {
             log.warn("Missing webhook token (posId={})", posId);
             return false;
         }
-        PosEntity posEntity = posRepository.findById(posId).orElse(null);
-        String decryptdb = encryptionService.decrypt(posEntity.getWebhookToken());
-        log.info("Decrypted DB token: {}", decryptdb);
-        log.info("Received webhook token: {}", webhookToken);
-
-        log.info("Checking webhook token for posId={}", posId);
         return posRepository.findById(posId)
                 .map(PosEntity::getWebhookToken)
                 .map(encryptionService::decrypt)
